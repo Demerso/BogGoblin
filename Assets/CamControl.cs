384d4aa73dbd6ce44de582fbe4352dde23b27a1a
ellioo <elliot.cr@gmail.com>
2020-10-10 19:24:29 -0400

    Basic Mvt

---
+++ Assets/CamControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+
+    public Vector3 offset;
+    public float pitch = 2f;
+    private float cZoom = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position - offset * cZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+    }
+}
---
+++ Assets/Clickable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+
+    public float range = 3f; // interactable range
+    bool isFocus = false;
+    Transform player;
+    bool interacted = false;
+    private void Update()
+    {
+        if (isFocus && interacted == false)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+            if(distance <= range)
+            {
+                Debug.Log("interact");
+                interacted = true;
+            }
+        }
+    }
+    public void OnFocused(Transform transform)
+    {
+        interact();
+        interacted = false;
+        isFocus = true;
+        player = transform;
+    }
+    public void DeFocus()
+    {
+        interacted = false;
+        isFocus = false;
+        player = null;  
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position,range);
+    }
+
+    public virtual void interact()
+    {
+        Debug.Log(this.name);
+    }
+}
