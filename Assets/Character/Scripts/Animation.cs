31aafb7670ac337e0c08afbde44caabcfae5a74a
Olivier Demers <olidem123@gmail.com>
2020-10-10 20:19:42 -0400

    Added basic skin and run animation

---
+++ Assets/Character/Scripts/Animation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Animation : MonoBehaviour
+{
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private NavMeshAgent navAgent;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var spd = navAgent.desiredVelocity.magnitude / navAgent.speed;
+        animator.SetFloat("Speed", spd);
+
+    }
+}
