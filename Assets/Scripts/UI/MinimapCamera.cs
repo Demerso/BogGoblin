286d17accb1ec07ae11387bc09af84b7b068c99b
Olivier Demers <olidem123@gmail.com>
2020-10-19 12:59:52 -0400

    Added minimap

---
+++ Assets/Scripts/UI/MinimapCamera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private void LateUpdate()
+    {
+        var newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
