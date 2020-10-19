de28eca757eb1ac2cbb03feb14b3b3e651fcbb33
Olivier Demers <olidem123@gmail.com>
2020-10-19 01:05:06 -0400

    Added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButton_OnClick()
+    {
+        SceneManager.LoadScene("PlayScene", LoadSceneMode.Single);
+    }
+
+    public void QuitButton_OnClick()
+    {
+        Application.Quit();
+    }
+}
